48805f484606de330eb3bafc7715fbacf0a27301
andreas <andreas.just.petersen@gmail.com>
2022-09-04 14:58:22 +0200

    Made bars show data

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -8,6 +8,14 @@ export default function BarChart(props) {
   const [sortBy, setSortBy] = useState("ascending");
   let [members, setMembers] = useState([...props.members].sort((a, b) => a.Age - b.Age));
 
+  var maxAge = 0;
+  members.forEach(m => {
+      maxAge = Math.max(maxAge, m.Age)
+  });
+
+  const barHeight = height - 20 - 100;
+  const barWidth = ((width - 60 - 30) / (members.length)) - 20
+
   function handleChange(e) {
     setSortBy(e.target.value);
 
@@ -36,8 +44,13 @@ export default function BarChart(props) {
         </div>
       </div>
       <div >
-            <svg className={styles.chart} width={width} height={height-60}>
-
+            <svg className={styles.chart} width={width} height={height-60} xmlns="http://www.w3.org/2000/svg">
+            
+                {members.map((m, i) => {
+                    return (<rect style={{fill: "#E38B29"}} width={barWidth} height={m.Age / maxAge * barHeight} x={60 + (i * (barWidth + 20))} y={height - 100 - (m.Age / maxAge * barHeight)}/>)
+                }) }
+                <line x1={40} y1={20} x2={40} y2={height - 100} stroke="black"/>
+                <line x1={40} y1={height - 100} x2={width - 30} y2={height - 100} stroke="black"/>
             </svg>
       </div>
     </div>
